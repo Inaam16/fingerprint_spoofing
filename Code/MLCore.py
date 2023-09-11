317cadfef09b4c06b4e861d1d30066d83623c6c9
Charbel Abi Daher <abidahercharbel@gmail.com>
2023-09-11 13:53:53 +0200

    Fixed linear regression main()

--- Code/MLCore.py
+++ Code/MLCore.py
@@ -22,6 +22,30 @@ def Z_score_eval(DTR, DTE):
     return (DTE - DTR.mean(1).reshape((DTR.shape[0], 1))) / (np.var(DTR, axis = 1).reshape((DTR.shape[0], 1)) ** 0.5)
 
 
+def principal_components(Data, dim):
+    """Apply Principal Component Analysis to the Data and return the dim most important dimensions"""
+    mu, cov = mean_and_covariance(Data)
+    eigen_values, eigen_vectors = np.linalg.eigh(cov)
+    dim_eigen_vects = eigen_vectors[:, ::-1][:, 0:dim]
+    dim_eigen_vals = eigen_values[::-1][0:dim]
+    return dim_eigen_vects, dim_eigen_vals
+
+
+def project_PCA(Data, eigen_vectors):
+    DTR = np.dot(eigen_vectors.T, Data)
+    return DTR
+
+
+def PCA(Data, dim, *, components=False):
+    """Return a new dataset projected in the new subspace
+    NEW: addded the dimEigVect as returned value"""
+    eigen_vectors, eigen_values = principal_components(Data, dim)
+    DTR = np.dot(eigen_vectors.T, Data)
+    if components:
+        return DTR, eigen_vectors, eigen_values
+    return DTR
+
+
 def PCA_preproccessor(DTR, LTR, DTE, LTE, dim):
     DTR_p, vects, _ = PCA(DTR, dim, components=True)
     DTE_p = project_PCA(DTE, vects)
--- Code/logistic_regression.py
+++ Code/logistic_regression.py
@@ -1,9 +1,5 @@
-
 """
-*** LOGISTIC REGRESSION ***
-
-
-
+LOGISTIC REGRESSION
 """
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
@@ -17,28 +13,37 @@ from functools import partial
 
 # Compute objective function and its derivatives (required by the optimization algorithm)
 def logreg_obj_wrap(DTR, LTR, l, pi_T):
-
     def logreg_obj(v):
         w, b = v[0:-1], v[-1]
 
         Nt = sum(LTR == 1)
         Nf = sum(LTR == 0)
 
-        J = l/2 * np.linalg.norm(w)**2 + pi_T / Nt * sum(np.log1p(np.exp( - (np.dot(w.T, DTR[:, LTR == 1]) + b )))) + \
-            (1 - pi_T) / Nf * sum(np.log1p(np.exp((np.dot(w.T, DTR[:, LTR == 0]) + b ))))
+        J = (
+            l / 2 * np.linalg.norm(w) ** 2
+            + pi_T / Nt * sum(np.log1p(np.exp(-(np.dot(w.T, DTR[:, LTR == 1]) + b))))
+            + (1 - pi_T) / Nf * sum(np.log1p(np.exp((np.dot(w.T, DTR[:, LTR == 0]) + b))))
+        )
 
-        dJw = l * w - pi_T / Nt * np.sum(DTR[:, LTR == 1]/(1+np.exp(np.dot(w.T, DTR[:, LTR == 1]) + b)), axis = 1) + \
-            (1-pi_T) / Nf * np.sum(DTR[:, LTR == 0]/(1+np.exp(-np.dot(w.T, DTR[:, LTR == 0]) - b)), axis = 1)
+        dJw = (
+            l * w
+            - pi_T
+            / Nt
+            * np.sum(DTR[:, LTR == 1] / (1 + np.exp(np.dot(w.T, DTR[:, LTR == 1]) + b)), axis=1)
+            + (1 - pi_T)
+            / Nf
+            * np.sum(DTR[:, LTR == 0] / (1 + np.exp(-np.dot(w.T, DTR[:, LTR == 0]) - b)), axis=1)
+        )
 
-        dJb = -pi_T/Nt * np.sum(1/(1+np.exp(np.dot(w.T, DTR[:, LTR == 1]) + b))) + \
-            (1-pi_T) / Nf * np.sum(1/(1+np.exp(-np.dot(w.T, DTR[:, LTR == 0]) - b)))
+        dJb = -pi_T / Nt * np.sum(1 / (1 + np.exp(np.dot(w.T, DTR[:, LTR == 1]) + b))) + (
+            1 - pi_T
+        ) / Nf * np.sum(1 / (1 + np.exp(-np.dot(w.T, DTR[:, LTR == 0]) - b)))
 
-        dJ = np.concatenate((dJw, np.array(dJb).reshape(1,)))
-        
+        dJ = np.concatenate((dJw,np.array(dJb).reshape(1,),))
+
         return J, dJ
 
     return logreg_obj
-
 
 
 # Train a linear logistic regression model and evaluate it on test data
@@ -46,7 +51,7 @@ def linear_logistic_regression(DTR, LTR, DTE, LTE, l, pi_T, pi, Cfn, Cfp):
     # Define objective function
     logreg_obj = logreg_obj_wrap(DTR, LTR, l, pi_T)
     # Optimize objective function
-    optV, _, _ = fmin_l_bfgs_b(logreg_obj, np.zeros(DTR.shape[0] + 1), approx_grad = False)   
+    optV, _, _ = fmin_l_bfgs_b(logreg_obj, np.zeros(DTR.shape[0] + 1), approx_grad=False)
 
     w, b = optV[0:-1], optV[-1]
 
@@ -57,26 +62,27 @@ def linear_logistic_regression(DTR, LTR, DTE, LTE, l, pi_T, pi, Cfn, Cfp):
 
     return s, minDCF
 
+
 # Map features to the quadratic feature space
 def map_to_feature_space(D):
-    phi = np.zeros([D.shape[0]**2+D.shape[0], D.shape[1]])
+    phi = np.zeros([D.shape[0] ** 2 + D.shape[0], D.shape[1]])
     for index in range(D.shape[1]):
         x = D[:, index].reshape(D.shape[0], 1)
         # phi = [vec(x*x^T), x]^T
-        phi[:, index] = np.concatenate((np.dot(x, x.T).reshape(x.shape[0]**2, 1), x)).reshape(phi.shape[0],)
+        phi[:, index] = np.concatenate((np.dot(x, x.T).reshape(x.shape[0] ** 2, 1), x)).reshape(
+            phi.shape[0],
+        )
     return phi
-
 
 
 # Train a quadratic logistic regression model and evaluate it on test data
 def quadratic_logistic_regression(DTR, LTR, DTE, LTE, l, pi_T, pi, Cfn, Cfp):
-
     # Map training features to expanded feature space
     phi = map_to_feature_space(DTR)
 
     # Train a linear regression model on expanded feature space
     logreg_obj = logreg_obj_wrap(phi, LTR, l, pi_T)
-    optV, _, _ = fmin_l_bfgs_b(logreg_obj, np.zeros(phi.shape[0] + 1), approx_grad = False)   
+    optV, _, _ = fmin_l_bfgs_b(logreg_obj, np.zeros(phi.shape[0] + 1), approx_grad=False)
     w, b = optV[0:-1], optV[-1]
 
     # Map test features to expanded feature space
@@ -88,32 +94,37 @@ def quadratic_logistic_regression(DTR, LTR, DTE, LTE, l, pi_T, pi, Cfn, Cfp):
 
     return s, minDCF
 
-# Perform k-fold cross validation on test data for the specified model
-def k_fold_cross_validation(D, L, classifier, k, pi, Cfp, Cfn, l, pi_T, seed = 0, preprocessor=None, just_llr = False):
 
+# Perform k-fold cross validation on test data for the specified model
+def k_fold_cross_validation(
+    D, L, classifier, k, pi, Cfp, Cfn, l, pi_T, seed=0, preprocessor=None, just_llr=False
+):
     np.random.seed(seed)
     idx = np.random.permutation(D.shape[1])
 
     start_index = 0
     elements = int(D.shape[1] / k)
 
-    llr = np.zeros([D.shape[1], ])
+    llr = np.zeros([D.shape[1],])
 
     for count in range(k):
         # Define training and test partitions
         if start_index + elements > D.shape[1]:
             end_index = D.shape[1]
         else:
-            end_index = start_index + elements 
+            end_index = start_index + elements
 
         idxTrain = np.concatenate((idx[0:start_index], idx[end_index:]))
         idxTest = idx[start_index:end_index]
 
         DTR = D[:, idxTrain]
         LTR = L[idxTrain]
-    
+
         DTE = D[:, idxTest]
         LTE = L[idxTest]
+
+        if preprocessor:
+            DTR, LTR, DTE, LTE = preprocessor(DTR, LTR, DTE, LTE)
 
         # Train the classifier and compute llr on the current partition
         llr[idxTest], _ = classifier(DTR, LTR, DTE, LTE, l, pi_T, pi, Cfn, Cfp)
@@ -128,73 +139,67 @@ def k_fold_cross_validation(D, L, classifier, k, pi, Cfp, Cfn, l, pi_T, seed = 0
     return minDCF, llr
 
 
-
 if __name__ == "__main__":
-
     ### Train and evaluate different logistic regression models using cross validation and single split
 
     for LR_type in ["linear", "quadratic"]:
-        D, L = load("../Train.txt")   
+        D, L = load("../Train.txt")
         DN = Z_score(D)
-        l_val = [0, 1e-6, 1e-4, 1e-2, 1, 100]
-        pi_T = 0.5 #to change later
-        img1 = "LR_lambda_kfold.png"
+        lambda_values = [0, 1e-6, 1e-4, 1e-2, 1, 100]
+        pca_values = list(range(5, 11))
+        pi_T = 0.5  # to change later
+        img_name = "LR_lambda_kfold.png"
         fileName = "../Results/LR_results.txt"
         linear_or_quadratic = linear_logistic_regression
+        plot_title = "Linear regression"
         if LR_type == "quadratic":
             fileName = "../Results/Quad_LR_results.txt"
             linear_or_quadratic = quadratic_logistic_regression
-            img1 = "Quad_LR_lambda_kfold.png"
+            img_name = "Quad_LR_lambda_kfold.png"
+            plot_title = "Quadratic regression"
 
         with open(fileName, "w") as f:
-             
             ### Raw features
             f.write("Values of min DCF for values of lambda = [0, 1e-6, 1e-4, 1e-2, 1, 100]\n")
             f.write("\nRaw features\n")
             DCF_kfold_raw = []
-            for l in l_val:
-                minDCF, _ = k_fold_cross_validation(D, L, linear_or_quadratic, 5, 0.5, 10, 1, l, pi_T, seed = 0)
+            for l in lambda_values:
+                minDCF, _ = k_fold_cross_validation(
+                    D, L, linear_or_quadratic, 5, 0.5, 10, 1, l, pi_T, seed=0
+                )
                 DCF_kfold_raw.append(minDCF)
-                f.write(f" L-val: {l} {minDCF}")
-
+                f.write(f"Lambda: {l} minDCF: {minDCF}\n")
 
             ### Z-normalized features - no PCA
             f.write("\nZ-normalized features - no PCA\n")
             DCF_kfold_z = []
-            for l in l_val:
-                minDCF, _ = k_fold_cross_validation(DN, L, linear_or_quadratic, 5, 0.5, 10, 1, l, pi_T, seed = 0)
+            for l in lambda_values:
+                minDCF, _ = k_fold_cross_validation(
+                    DN, L, linear_or_quadratic, 5, 0.5, 10, 1, l, pi_T, seed=0
+                )
                 DCF_kfold_z.append(minDCF)
-                f.write(f" L-val: {l} {minDCF}")
-   
+                f.write(f"Lambda: {l} minDCF: {minDCF}\n")
+
             ### Z-normalized features - PCA = 9 ( chose our pca value)
             f.write("\nZ-normalized features - PCA = 9\n")
-            DCF_kfold_z_pca = []
-            for l in l_val:
-                values = []
-                for i in range(5, 11):
+            DCF_kfold_z_pca = {k: list() for k in pca_values}
+            for l in lambda_values:
+                for i in pca_values:
                     preprocessor = partial(PCA_preproccessor, dim=i)
-                    values.append(
-                        k_fold_cross_validation(
-                            D, L, linear_or_quadratic, 5, 0.5, 10, 1, l,pi_T,seed=0, preprocessor=preprocessor
-                        )
-                    )
-                    f.write(f"l_val:{l} PCA:{i} {values[-1]}")
+                    minDCF, _ = k_fold_cross_validation(D, L, linear_or_quadratic, 5, 0.5, 10, 1, l, 
+                                                        pi_T, preprocessor=preprocessor)
+                    DCF_kfold_z_pca[i].append(minDCF)
+                    f.write(f"Lambda: {l} PCA dim:{i} minDCF {minDCF}\n")
 
-            
- 
             ### Plot min DCF for different values of lambda
             plt.figure()
-            plt.plot(l_val, DCF_kfold_raw)
-            plt.plot(l_val, DCF_kfold_z)
+            plt.plot(lambda_values, DCF_kfold_raw, label="Raw")
+            plt.plot(lambda_values, DCF_kfold_z, label="Z-normalized")
+            for i in pca_values:
+                plt.plot(lambda_values, DCF_kfold_z_pca[i], label=f"Z-normalized, PCA {i}")
             plt.xscale("log")
             plt.xlabel(r"$\lambda$")
             plt.ylabel("min DCF")
-            plt.legend(["Raw", "Z-normalized", "Gaussianized"])
-            plt.savefig("../Images/" + img1)
-
-
-            
-            
-            
-            
-    
+            plt.legend()
+            plt.title(plot_title)
+            plt.savefig("../Visualization/" + img_name)
