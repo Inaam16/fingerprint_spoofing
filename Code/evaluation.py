2b49294457b1a882f0eea2abe50a99e208eb914d
Maria Akl <maria.akl@waveinformatica.com>
2023-09-17 01:45:21 +0200

    fixed bug in evaluation file

--- Code/evaluation.py
+++ Code/evaluation.py
@@ -33,23 +33,22 @@ if __name__ == "__main__":
     K_SVM = 1
     
     ### Evaluating LR
-   
-    filename = "../Results/Evaluation/Eval_LR_8.txt"
+    # filename = "../Results/Evaluation/Eval_LR_8.txt"
 
-    lambda_values = [0, 1e-6, 1e-4, 1e-2, 1, 100]
-    with open(filename, "w") as f:
-        for pca in [8]:
-            f.write(f"{pca} \n")
-            if pca == 8:
-                DTR = DTR_8
-                DTE = DTE_8
-            else:
-                DTR = DTR
-                DTE = DTE
-            for l in lambda_values:
-                _, minDCF_lr = lr.quadratic_logistic_regression(DTR, LTR, DTE, LTE, l, 1/11, 1/11, 1, 1)
-                f.write(f"lambda : {l} , minDCF: {minDCF_lr} \n ")
-                print(minDCF_lr)
+    # lambda_values = [0, 1e-6, 1e-4, 1e-2, 1, 100]
+    # with open(filename, "w") as f:
+    #     for pca in [8]:
+    #         f.write(f"{pca} \n")
+    #         if pca == 8:
+    #             DTR = DTR_8
+    #             DTE = DTE_8
+    #         else:
+    #             DTR = DTR
+    #             DTE = DTE
+    #         for l in lambda_values:
+    #             _, minDCF_lr = lr.quadratic_logistic_regression(DTR, LTR, DTE, LTE, l, 1/11, 1/11, 1, 1)
+    #             f.write(f"lambda : {l} , minDCF: {minDCF_lr} \n ")
+    #             print(minDCF_lr)
 
 
 
@@ -88,25 +87,29 @@ if __name__ == "__main__":
                             print(minDCF_svm)
 
 
-    filename = "./Results/Evaluation/Eval_GMM"
 
-    with open(filename, "w") as f:
-        f.write(f"PCA: {pca} \n")
-        for pca in [6, None]:
-            if pca == 6:
-                DT = DTR_6
-                DE = DTE_6
-            else:
-                DT = DTR
-                DE = DTE
-            for tied0 in [True, False]:
-                f.write(f"Tied0: {tied0}")
-                for tied1 in [True, False]:
-                    f.write(f"Tied1: {tied1}")
-                    for n0 in [4,8]:
-                        _, minDCF_gmm = GMM.GMM_classifier_1(DTR, LTR, DTE, LTE, 2, n0, 2, True, tied0, True, tied1, 1/11, 1, 1)
-                        f.write(f"({n0},2) min DCF: {minDCF_gmm} \n")
-                        print(minDCF_gmm)
+
+
+     ### EVAL GMM ###
+    # filename = "./Results/Evaluation/Eval_GMM"
+
+    # with open(filename, "w") as f:
+    #     f.write(f"PCA: {pca} \n")
+    #     for pca in [6, None]:
+    #         if pca == 6:
+    #             DT = DTR_6
+    #             DE = DTE_6
+    #         else:
+    #             DT = DTR
+    #             DE = DTE
+    #         for tied0 in [True, False]:
+    #             f.write(f"Tied0: {tied0}")
+    #             for tied1 in [True, False]:
+    #                 f.write(f"Tied1: {tied1}")
+    #                 for n0 in [4,8]:
+    #                     _, minDCF_gmm = GMM.GMM_classifier_1(DTR, LTR, DTE, LTE, 2, n0, 2, True, tied0, True, tied1, 1/11, 1, 1)
+    #                     f.write(f"({n0},2) min DCF: {minDCF_gmm} \n")
+    #                     print(minDCF_gmm)
 
         
 
